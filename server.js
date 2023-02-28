4b4764178d98b131160b37e1683302ae5f9f4aa8
nikola.bozin.org@outlook.com <nikola.bozin.org@outlook.com>
2023-02-28 21:54:17 +0100

    fix

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.post('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at ${app.address}:${port}`);
 });
