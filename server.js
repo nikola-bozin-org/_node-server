88b88c073599921e329bec0495d0f3756246c77f
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-03-08 13:40:14 +0100

    rate-limiter

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const ip = require('ip');
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const router = require('./routes')
 const server = express();
 
-
+//2 requests every 15 minutes.
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 2,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 const port = 9876;
 
-const approvedOrigins = ['http://localhost:3000',]
+const approvedOrigins = ['http://localhost:9876',]
 server.use(cors({
   origin: approvedOrigins
 }));
 server.use(express.json()); 
 server.use(express.urlencoded({ extended: true })); 
 server.use(helmet());
+server.use(limiter);
 server.use('/api',router);
 
 
