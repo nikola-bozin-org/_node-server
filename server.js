fdf71c6e3350a169a31e1ff6d2312b5c2c4bee7f
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-04-02 08:38:30 +0200

    minimal

--- server.js
+++ server.js
@@ -1,40 +1,21 @@
-const ip = require('ip');
-const rateLimit = require('express-rate-limit');
 const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const router = require('./routers/route')
 const server = express();
 
-//2 requests every 15 minutes.
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 6,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
+
 const port = 9876;
-const authorizationToken = (req,res,next) =>{
-  const token = req.headers.authorization_token;
-  if(!token) return res.status(400).json({error:`JWT 'authorizationToken' entry is not present in request headers.`}); 
-  next();
-}
 
 
-const approvedOrigins = ['http://localhost:9876',]
-server.use(cors({
-  origin: approvedOrigins
-}));
+
 server.use(express.json()); 
 server.use(express.urlencoded({ extended: true })); 
-server.use(helmet());
-server.use(limiter);
-// server.use(authorizationToken);
-server.use('/api',router);
+
+
+server.get('/',(req,res)=>{res.status(400).json({message:"Hello."})})
+
 
 const startServer = async()=>{
   server.listen(port, () => {
-    console.log(`Server listening at ${ip.address()}:${port}`);
+    console.log(`Server listening at PORT: ${port}`);
   });
 }
 
--- statusCodes.js
+++
@@ -1,8 +0,0 @@
-const statusCode = {
-    OK: 200,
-    ERROR: 400,
-    UNAUTHORIZED: 401,
-    NOT_FOUND:404,
-    INTERNAL_SERVER_ERROR:500
-}
-module.exports=statusCode;
